368db90ecc3261e7a00d04c3e62a6d8eedcd6d56
MayzaHirose <maayhirose@gmail.com>
2018-07-04 21:05:51 -0300

    mais algumas configuracoes

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from sklearn import tree
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('voice.csv')
-df = df.sample(frac=1).reset_index(drop=True)
+df_shuffle = df.sample(frac=1).reset_index(drop=True)
 
 # train, test  = train_test_split (df, test_size=0.4)
 # clf = svm.SVC(kernel='linear', C=1).fit(train)
-a = df.drop('label', axis=1)
-test = df['label'].values
+
+a = df_shuffle.drop('label', axis=1)
+test = df_shuffle['label'].values
 
 
 # clf = tree.DecisionTreeClassifier() 
@@ -29,26 +30,27 @@ test = df['label'].values
 # print m
 # print f
 
+
 X_train, X_test, y_train, y_test = train_test_split (a, test, test_size=0.4, random_state=0)
-clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
+clf = svm.SVC(kernel='poly', C=1e3).fit(X_train, y_train)
 
 predicao = clf.predict(X_test)
 counter = 0
 acertos = 0
 erros = 0
 for p in predicao:
-    if p == test[counter]:
+    if p == y_test[counter]:
         acertos = acertos + 1
     else:
         erros = erros + 1
-    print "disse: ", p
-    print "certo: ", test[counter]
+
+    # print "disse: ", p
+    # print "certo: ", y_test[counter]
     counter = counter + 1
 
 print clf.score(X_test, y_test)
 print "TOTAL ACERTOS: ", acertos
 print "TOTAL ERROS: ", erros
-print 
 
 
 # features = [[140, 1], [130, 1],
